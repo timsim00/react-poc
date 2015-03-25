8b39729498a770be870408d64f75a7f0316022e1
diasommerer <dsommerer@exacttarget.com>
2015-03-25 13:52:54 -0400

    updates to tags, type, entitlement for emails.js

--- app/data/emails.js
+++ app/data/emails.js
@@ -8,8 +8,9 @@ module.exports = [{
     createDate: "March 12, 2015 4:36:05 PM",
     modifiedDate: "March 20, 2015 2:17:17 PM",
     folder: 3,
-    tags: [],
-    type: [1]
+    tags: [2],
+    type: [1],
+    entitlements: [6]
 }, {
     id: 2,
     name: "MS - Newsletter - February 2015",
@@ -20,9 +21,9 @@ module.exports = [{
     createDate: "February 12, 2015 2:07:38 PM",
     modifiedDate: "February 12, 2015 2:26:10 PM",
     folder: 3,
-    tags: [],
+    tags: [2],
     type: [1],
-    entitlements: []
+    entitlements: [6]
 }, {
     id: 3,
     name: "Mortgage Newsletter - March",
@@ -33,9 +34,9 @@ module.exports = [{
     createDate: "January 14, 2015 6:17:48 PM",
     modifiedDate: "February 24, 2015 1:33:13 PM",
     folder: 8,
-    tags: [],
+    tags: [2,9],
     type: [1],
-    entitlements: []
+    entitlements: [4]
 }, {
     id: 4,
     name: "Retirement Newsletter - February",
@@ -46,9 +47,9 @@ module.exports = [{
     createDate: "January 28, 2015 11:31:21 AM",
     modifiedDate: "February 10, 2015 11:39:12 AM",
     folder: 7,
-    tags: [],
+    tags: [2,8],
     type: [1],
-    entitlements: []
+    entitlements: [5]
 }, {
     id: 5,
     name: "Retirement Newsletter - January",
@@ -59,9 +60,9 @@ module.exports = [{
     createDate: "January 8, 2015 11:34:39 AM",
     modifiedDate: "January 21, 2015 11:34:39 AM",
     folder: 7,
-    tags: [],
+    tags: [1,2],
     type: [1],
-    entitlements: []
+    entitlements: [5]
 }, {
     id: 6,
     name: "Retirement Newsletter - December",
@@ -72,9 +73,9 @@ module.exports = [{
     createDate: "December 15, 2014 11:35:01 AM",
     modifiedDate: "December 16, 2014 11:45:16 AM",
     folder: 7,
-    tags: [],
+    tags: [1,2],
     type: [1],
-    entitlements: []
+    entitlements: [5]
 }, {
     id: 7,
     name: "Retirement Newsletter - November",
@@ -85,9 +86,9 @@ module.exports = [{
     createDate: "November 15, 2014 11:35:21 AM",
     modifiedDate: "November 17, 2014 11:42:05 AM",
     folder: 7,
-    tags: [],
+    tags: [1,2],
     type: [1],
-    entitlements: []
+    entitlements: [5]
 }, {
     id: 8,
     name: "Confirmation - Webinar Registration",
@@ -98,9 +99,9 @@ module.exports = [{
     createDate: "March 10, 2015 11:35:21 AM",
     modifiedDate: "March 15, 2015 11:42:05 AM",
     folder: 4,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [4],
+    type: [2],
+    entitlements: [6]
 }, {
     id: 9,
     name: "Invite - Investing 101 Webinar",
@@ -111,9 +112,9 @@ module.exports = [{
     createDate: "March 12, 2015 1:15:19 PM",
     modifiedDate: "March 15, 2015 1:25:40 PM",
     folder: 4,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [4,2],
+    type: [2],
+    entitlements: [6]
 }, {
     id: 10,
     name: "Invite - Real Estate 101 Webinar",
@@ -124,9 +125,9 @@ module.exports = [{
     createDate: "February 14, 2015 1:19:32 PM",
     modifiedDate: "March 12, 2015 1:26:42 PM",
     folder: 4,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [9],
+    type: [2],
+    entitlements: [4]
 }, {
     id: 11,
     name: "Invite - Saving for College",
@@ -137,9 +138,9 @@ module.exports = [{
     createDate: "February 18, 2015 1:01:05 PM",
     modifiedDate: "March 6, 2015 1:27:59 PM",
     folder: 4,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [7],
+    type: [3],
+    entitlements: [6]
 }, {
     id: 12,
     name: "On the Market - White Paper",
@@ -150,9 +151,9 @@ module.exports = [{
     createDate: "March 15, 2015 2:53:21 PM",
     modifiedDate: "March 20, 2015 3:07:17 PM",
     folder: 5,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [5],
+    type: [3],
+    entitlements: [1]
 }, {
     id: 13,
     name: "The 1% Move - White Paper",
@@ -163,9 +164,9 @@ module.exports = [{
     createDate: "February 12, 2015 2:48:41 PM",
     modifiedDate: "March 8, 2015 3:07:17 PM",
     folder: 5,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [4,5],
+    type: [3],
+    entitlements: [1]
 }, {
     id: 14,
     name: "Securities Updates",
@@ -176,7 +177,7 @@ module.exports = [{
     createDate: "February 18, 2015 3:41:23 PM",
     modifiedDate: "February 24, 2015 3:42:13 PM",
     folder: 6,
-    tags: [],
-    type: [],
-    entitlements: []
+    tags: [4,5],
+    type: [2],
+    entitlements: [2]
 }];
